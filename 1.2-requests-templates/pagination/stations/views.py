4f93487bfb0abd9784f0d6229ff4cab4653e6875
Anton Zykov <sonic1988@list.ru>
2024-07-02 12:26:39 +0700

    changes in views.py (homework pagination)

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,17 +1,26 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
-
+from django.conf import settings
+import csv
 
 def index(request):
     return redirect(reverse('bus_stations'))
 
 
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
+    csv_file_path = settings.BUS_STATION_CSV
+    table = []
+    with open(csv_file_path, encoding='utf-8') as csvfile:
+        data = csv.DictReader(csvfile, delimiter=",")
+        table = list(data)
 
+    paginator = Paginator(table, 10)
+    current_page = request.GET.get('page', 1)
+    page = paginator.get_page(current_page)
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+    'page': page,
+    'bus_stations': page.object_list
     }
+
     return render(request, 'stations/index.html', context)
